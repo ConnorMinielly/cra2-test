09ee474f865ac0a827485a5fc3c406d2d039a602
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-10-29 16:26:40 -0400

    make header a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import { Icon } from 'semantic-ui-react';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <Icon
-          name="american sign language interpreting"
-          size="large"
-          style={{ color: 'white' }}
-        />
-      </header>
-    );
-  }
-}
+const Header = () => (
+  <header className="header">
+    <Icon
+      name="american sign language interpreting"
+      size="large"
+      style={{ color: 'white' }}
+    />
+  </header>
+);
 
 export default Header;
